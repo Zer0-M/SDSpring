608acd0f69087de5edcd24bca6e17ad19a133315
Zer0-M <mjamil90@stuy.edu>
2019-01-30 14:30:03 -0500

    added work0

---
+++ 00_canvas/clickdraw.js
@@ -0,0 +1,47 @@
+var canvas=document.getElementById("slate");
+var clearb=document.getElementById("clear");
+var rectb=document.getElementById("rectmode");
+var dotb=document.getElementById("dotmode");
+var ctx=canvas.getContext("2d");
+var rectangle=true;
+clearb.addEventListener(
+    "click",
+    function(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+    }
+)
+canvas.addEventListener(
+    "click",
+    function(e){
+        console.log(e);
+        var rect=canvas.getBoundingClientRect();
+        if(rectangle){
+           ctx.fillRect(e.pageX-rect.left,e.pageY-rect.top,10,10);
+        }
+        else{
+            console.log(e.pageX-rect.left);
+            ctx.beginPath();
+            ctx.ellipse(e.pageX-rect.left,e.pageY-rect.top,5,5,0, 0, 2 * Math.PI);
+            ctx.fill()
+            ctx.stroke();
+        }
+    }
+)
+dotb.addEventListener(
+    "click",
+    ()=>{
+        rectangle=false;
+        rectb.removeAttribute("hidden");
+        dotb.setAttribute("hidden",true);
+    }
+)
+rectb.addEventListener(
+    "click",
+    ()=>{
+        rectangle=true;
+        dotb.removeAttribute("hidden");
+        rectb.setAttribute("hidden",true);
+    }
+)
+
+
