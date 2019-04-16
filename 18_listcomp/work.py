5f0fd246e89bc84e4b64b0f01d1242600b9e971d
Zer0-M <mjamil90@stuy.edu>
2019-04-16 18:56:00 -0400

    added work19

--- 18_listcomp/work.py
+++ 18_listcomp/work.py
@@ -1,14 +1,19 @@
 '''
 Mohammed Jamil
 SoftDev2 pd 6
-K18 --
+K18 -- Getting Clever with List Comprehensions
 2019-04-16
 '''
+import random
 def triples(n):
     l=[(x,y,int((x**2+y**2)**0.5)) for x in range(1,n) for y in range(x+1,n) if ((x**2+y**2)**0.5).is_integer() and ((x**2+y**2)**0.5)<=n]
     return l
 print(triples(30))
 def quicksort(l):
-    L=[quicksort([x for x in l if x<l[0]])+[z for z in l if z==l[0]]+quicksort([y for y in l if y>l[0]]) if len(l)>1 else l][0]
-    return L
-print(quicksort([5,2,3,6,899,21,44,1,6,9,13,31,41,61,-13,536,2,-4]))
+    return [quicksort([x for x in l if x<l[0]])+[z for z in l if z==l[0]]+quicksort([y for y in l if y>l[0]]) if len(l)>1 else l][0]
+L=[]
+for i in range(0,10000):
+    L.append(random.randint(0,100000000))
+sortd=quicksort(L)
+L.sort()
+print(sortd==L)
