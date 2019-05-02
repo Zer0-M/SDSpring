c69eeb4c1731122353a5daba586d5e2da095127a
Zer0-M <mjamil90@stuy.edu>
2019-05-02 11:53:10 -0400

    added work 23

---
+++ 23_memoization/work.py
@@ -0,0 +1,37 @@
+'''
+Mohammed Jamil
+SoftDev2 pd6
+K23 -- Memoization
+2019-02-05  
+'''
+
+import random
+def make_HTML_heading(f):
+    txt=f()
+    def inner():
+        print(txt)
+        return '<h1>' + txt + '</h1>'
+    return inner
+@make_HTML_heading
+def greet():
+    greetings=['Hello','Welcome','AYO!','Hola','Bonjour','Word up']
+    return random.choice(greetings)
+print(greet())
+
+def memoize(f):
+    memo={}
+    def helper(x):
+        nonlocal memo
+        if x not in memo:
+            memo[x]=f(x)
+        return memo[x]
+    return helper
+@memoize
+def fib(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+print(fib(40))
